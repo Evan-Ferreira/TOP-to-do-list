5c9bcc8951de6c6f2a095f071da94cd97b8c74f8
Evan Ferreira <evanjfer@gmail.com>
2024-12-28 22:05:13 -0500

    Finish styling for project bar
    - style project bar
    - style whole page
    - made project bar responsive (open and close)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './styles.css';
+
+const initializeProperties = (function () {
+    const projectCloseBtn = document.querySelector('.project-close-btn');
+    const project = document.querySelector('.project');
+    let projectBarOpen = true;
+    const onclickProps = () => {
+        projectCloseBtn.addEventListener('click', () => {
+            const projectBar = document.getElementsByClassName('project-bar');
+            projectBar[0].setAttribute(
+                'style',
+                projectBarOpen ? 'display: none' : 'display: block'
+            );
+            projectBarOpen ? (projectBarOpen = false) : (projectBarOpen = true);
+        });
+    };
+    onclickProps();
+})();
+
+class DOM {
+    constructor() {
+        this.projectCloseBtn = document.querySelector('.project-close-btn');
+        this.element = document.querySelector(selector);
+    }
+
+    on(eventType, callback) {
+        this.element.addEventListener(eventType, callback);
+    }
+}
