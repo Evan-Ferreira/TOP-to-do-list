1a89b1880ce3c20009e15f5c3f84d0f39c0826ba
Evan Ferreira <evanjfer@gmail.com>
2024-12-28 23:36:19 -0500

    Finish the javascript for add/remove projects
    - Add event listener to the add project button
    - Add event listener to the remove project button
    - completed logic for adding and removing projects,
    activated from given event listener

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import './styles.css';
 
+const projectMap = new Map();
+let selectedProject = '';
+
+class DOM {
+    newProject(projectName) {
+        const projectBar = document.querySelector('.project-bar');
+        const newProject = document.createElement('div');
+        newProject.textContent = projectName;
+        newProject.classList.add('project');
+        newProject.setAttribute('id', projectName);
+        newProject.addEventListener('click', () => {
+            selectedProject = projectName;
+        });
+        projectBar.appendChild(newProject);
+    }
+
+    removeProject() {
+        const project = document.getElementById(selectedProject);
+        project.remove();
+    }
+}
+
 const initializeProperties = (function () {
+    const dom = new DOM();
     const projectCloseBtn = document.querySelector('.project-close-btn');
-    const project = document.querySelector('.project');
+    const projectBar = document.getElementsByClassName('project-bar');
+    const newProjectBtn = document.querySelector('.new-project');
+    const deleteProjectBtn = document.querySelector('.delete-project');
     let projectBarOpen = true;
+
     const onclickProps = () => {
         projectCloseBtn.addEventListener('click', () => {
-            const projectBar = document.getElementsByClassName('project-bar');
             projectBar[0].setAttribute(
                 'style',
                 projectBarOpen ? 'display: none' : 'display: block'
@@ -15,15 +40,29 @@ const initializeProperties = (function () {
         });
     };
     onclickProps();
+
+    const createProject = () => {
+        newProjectBtn.addEventListener('click', () => {
+            console.log('penis');
+            const project = new Project();
+            projectMap.set(project.name, {});
+            dom.newProject(project.name);
+        });
+    };
+    createProject();
+
+    const removeProject = () => {
+        deleteProjectBtn.addEventListener('click', () => {
+            if (selectedProject) {
+                dom.removeProject();
+            }
+        });
+    };
+    removeProject();
 })();
 
-class DOM {
+class Project {
     constructor() {
-        this.projectCloseBtn = document.querySelector('.project-close-btn');
-        this.element = document.querySelector(selector);
-    }
-
-    on(eventType, callback) {
-        this.element.addEventListener(eventType, callback);
+        this.name = prompt('Enter the project name:');
     }
 }
